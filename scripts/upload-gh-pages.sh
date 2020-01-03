f8c9a314af28fb1bf48e3e397f666f68a138e1f8
Kumuditha Karunarathna <kumudithaudaya@gmail.com>
2020-01-03 20:22:38 +0530

    Build Phiphme site

--- scripts/upload-gh-pages.sh
+++ scripts/upload-gh-pages.sh
@@ -6,6 +6,9 @@ git config --global user.email "noreply+travis@fossasia.org"
 cd docs/sources
 
 make html
+mkdir _build/html/docs
+cp -r ../images _build/html/docs
+cp -r ../../fastlane _build/html
 
 git clone --quiet --branch=gh-pages https://kumuditha-udayanga:$GITHUB_API_KEY@github.com/kumuditha-udayanga/phimpme-android gh-pages > /dev/null
 cd gh-pages
